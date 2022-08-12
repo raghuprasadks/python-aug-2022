a1327c71822a5b3c1e7385e6bbf23a173b404a79
raghu prasad ks <prasadraghuks@gmail.com>
2022-08-12 10:27:40 +0530

    updated

--- programs/12-markssheetdict.py
+++ programs/12-markssheetdict.py
@@ -7,15 +7,22 @@ marksdict={100:["ranga",20,23,18],
            }
 print("marksdict ",marksdict)
 print("Total marks of every student")
-
+toppermarks=0
+topper=""
 for k,v in marksdict.items():
     print("key ",k," value ",v)
     name = v[0]
     total = v[1]+v[2]+v[3]
-
-    """
-    for m in v:
-        name=v[0]
-        total = v[1]+v[2]+v[3]
-    """
     print(name,total)
+    if (total>toppermarks):
+        toppermarks=total
+        topper=name
+
+print("topper ",topper, "topper marks ",toppermarks)
+"""
+Display - topper
+subjectwise topper
+"""
+lst=[20,30,40,50]
+lst1 =lst[1:3]
+print("lst 1",lst1)
