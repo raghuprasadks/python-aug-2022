b36432457fa5bc67efe67e5f4fde0bd76f97a882
raghu prasad ks <prasadraghuks@gmail.com>
2022-08-13 10:57:54 +0530

    updated

---
+++ programs/16-TableCreation.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import sqlite3
+
+conn = sqlite3.connect('ecommabbs.db')
+print("Opened database successfully")
+
+conn.execute('''CREATE TABLE if not exists COMPANY 
+         (ID INT PRIMARY KEY     NOT NULL,
+         NAME           TEXT    NOT NULL,
+         AGE            INT     NOT NULL,
+         ADDRESS        CHAR(50),
+         SALARY         REAL)''')
+
+print("Table created successfully")
+
+conn.close()
